bc2561eab7f1c0e870c1da4f1359be69dc328fae
Kevin Iverson <kgiverson@yahoo.com>
2015-03-08 11:20:59 -0500

    Added the ability to select which path to blame for the clash

--- clash_util.py
+++ clash_util.py
@@ -21,6 +21,8 @@ parser = SafeConfigParser()
 parser.read(config_file)
 #print parser.get('path', 'path_file1')
 
+CSV_HEADER = "CLASH_GROUP_NAME, ORIGIN_CLASH, CLASH_GROUP_COUNT, TOTAL_CLASHES, PATH_COMBO, PATH_BLAME, CLASH_DETAIL\n"
+
 # create a list with path priority order
 path_order = []
 for path_order_num, path_file_name in parser.items("path"):
@@ -35,7 +37,6 @@ clash_count = 0
 # first some summary information
 for item in doc.iterfind('batchtest/clashtests/clashtest/summary'):
     clash_count = item.attrib['total']
-    print clash_count
 
 # now some detail information about the clashes
 for item in doc.iterfind('batchtest/clashtests/clashtest/clashresults/clashresult'):
@@ -66,16 +67,42 @@ results = {}
 for x in parsed_data:
     group_size = 1
     group_file_key = ""
+
     clash1_image = parsed_data[x][0]
     clash1_name = parsed_data[x][1]
     clash1_file_key = parsed_data[x][2]
     clash1_grid_line = parsed_data[x][3]
+    group_path_blame = ""
+    # based on the precedence in the configuration file we need to attribute
+    # this clash to one of the sub systems
+    # first separate into two paths
+    path1, path2 = clash1_file_key.split('-')
+    # loop through path order.  We could use index, but we want to sub search
+    # so doing this the hard way
+    found = 0
+    for p in path_order:
+        # see if that path_order text is in either paths
+        if p in path1:
+            found = 1
+            group_path_blame = path1
+        elif p in path2:
+            found = 1
+            group_path_blame = path2
+
+        if found:
+            break
+
+    # if we get here an haven't found anything our config is messed up, warn the user
+    if not found:
+        print "Could not find path order for %s.  Please check your config file." % clash1_file_key
+
     z1 = x[2]
     y1 = x[1]
     x1 = x[0]
     finds_key = [clash1_name]
     finds_source = clash1_name
-    finds_data = [group_size, group_file_key, clash1_name, (clash1_name, clash1_image, clash1_grid_line)]
+    finds_data = [group_size, group_file_key, clash1_name, group_path_blame, (clash1_name, clash1_image, clash1_grid_line)]
+
     for y in parsed_data:
         clash2_image = parsed_data[y][0]
         clash2_name = parsed_data[y][1]
@@ -87,28 +114,6 @@ for x in parsed_data:
         # skip if we are not comparing two clashes from the same sub system combination
         if clash1_file_key != clash2_file_key:
             continue
-        # TODO - based on the precedence in the configuration we also need to attribute
-        # this clash to one of the sub systems
-        # first separate into two paths
-        path1, path2 = clash1_file_key.split('-')
-        print '1: %s   2: %s' % (path1,path2)
-        # loop through path order.  We could use index, but we want to sub search
-        # so doing this the hard way
-        found = 0
-        for p in path_order:
-            # see if that path_order text is in either paths
-            if path1.find(p):
-                found = 1
-                print 'found path1'
-                print path1
-            elif path2.find(p):
-                found = 1
-                print 'found path2'
-                print path2
-
-            if found:
-                break
-
 
         finds_data[1] = clash1_file_key
         z2 = y[2]
@@ -120,22 +125,31 @@ for x in parsed_data:
             if (ydelt >= -box_size) and (ydelt <= box_size):
                 xdelt = float(x1)-float(x2)
                 if (xdelt >= -box_size) and (xdelt <= box_size):
+                    # found a clash  we care about
                     finds_key.append((clash2_name))
                     finds_data.append((clash2_name, clash2_image, clash2_grid_line))
                     finds_data[0] += 1
+
+
     if len(finds_key) > 1:
         s_finds_key = sorted(finds_key)
         s_finds_key_t = tuple(s_finds_key)
         if s_finds_key_t not in results.keys():
             results[s_finds_key_t] = finds_data
 
-print len(results)
-writer = csv.writer(open(clash_output_filename, 'wb'))
-#TODO - add a header row
+print "Found %s clash groups in %s total clashes" % (len(results), clash_count)
+
+output_file = open(clash_output_filename, 'wb')
+# add a header row
+output_file.write(CSV_HEADER)
+writer = csv.writer(output_file)
 for clash_group, clash_data in results.items():
     print clash_group
     group_count = clash_data.pop(0)
     group_file_key = clash_data.pop(0)
     group_origin_clash = clash_data.pop(0)
-    writer.writerow([clash_group, group_origin_clash, group_count, clash_count, group_file_key, clash_data])
+    group_path_blame = clash_data.pop(0)
+    writer.writerow([clash_group, group_origin_clash, group_count, clash_count, group_file_key, group_path_blame, clash_data])
+
+output_file.close()
 
