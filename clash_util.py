8da157902c0b877266122d0e6235499e8aa5c034
Kevin Iverson <kgiverson@yahoo.com>
2015-03-08 10:29:45 -0500

    Initial config stuff. More to come

--- clash_util.py
+++ clash_util.py
@@ -1,4 +1,5 @@
 from xml.etree.ElementTree import parse
+from ConfigParser import SafeConfigParser
 import csv
 
 #first lets store the data
@@ -9,14 +10,34 @@ import csv
 #doc = parse('test_data/Clash_Test_All_System_Clash.xml')
 doc = parse('test_data/Structural_vs_ALL_Grid_Update.xml')
 clash_output_filename = 'clash_group.csv'
+#TODO parmeterize box_size
+box_size = 3.0
+#TODO parameterize the config file
+config_file="clash_util.ini"
+#TODO output help information
+
+#TODO - need to pull data from configuration file
+parser = SafeConfigParser()
+parser.read(config_file)
+#print parser.get('path', 'path_file1')
+
+# create a list with path priority order
+path_order = []
+for path_order_num, path_file_name in parser.items("path"):
+    path_order.append(path_file_name)
+    #print ' %s = %s' % (path_order_num, path_file_name)
+#print path_order
 
 parsed_data = {}
 clash_count = 0
 
+# parse the XML data we care about
+# first some summary information
 for item in doc.iterfind('batchtest/clashtests/clashtest/summary'):
     clash_count = item.attrib['total']
     print clash_count
 
+# now some detail information about the clashes
 for item in doc.iterfind('batchtest/clashtests/clashtest/clashresults/clashresult'):
     imagefile = item.attrib['href']
     name = item.attrib['name']
@@ -40,9 +61,7 @@ for item in doc.iterfind('batchtest/clashtests/clashtest/clashresults/clashresul
         coord = (x,y,z)
         parsed_data[coord] = (imagefile,name,file_keys, grid_location)
 
-#now let's iterate and find the ones within a certain distance
-#TODO - make this a command line parameter
-box_size = 3.0
+# now let's iterate and find the ones within a certain distance
 results = {}
 for x in parsed_data:
     group_size = 1
@@ -62,10 +81,35 @@ for x in parsed_data:
         clash2_name = parsed_data[y][1]
         clash2_file_key = parsed_data[y][2]
         clash2_grid_line = parsed_data[y][3]
+        # skip if we are comparing a clash to iteself
         if clash1_name == clash2_name:
             continue
+        # skip if we are not comparing two clashes from the same sub system combination
         if clash1_file_key != clash2_file_key:
             continue
+        # TODO - based on the precedence in the configuration we also need to attribute
+        # this clash to one of the sub systems
+        # first separate into two paths
+        path1, path2 = clash1_file_key.split('-')
+        print '1: %s   2: %s' % (path1,path2)
+        # loop through path order.  We could use index, but we want to sub search
+        # so doing this the hard way
+        found = 0
+        for p in path_order:
+            # see if that path_order text is in either paths
+            if path1.find(p):
+                found = 1
+                print 'found path1'
+                print path1
+            elif path2.find(p):
+                found = 1
+                print 'found path2'
+                print path2
+
+            if found:
+                break
+
+
         finds_data[1] = clash1_file_key
         z2 = y[2]
         y2 = y[1]
@@ -87,6 +131,7 @@ for x in parsed_data:
 
 print len(results)
 writer = csv.writer(open(clash_output_filename, 'wb'))
+#TODO - add a header row
 for clash_group, clash_data in results.items():
     print clash_group
     group_count = clash_data.pop(0)
