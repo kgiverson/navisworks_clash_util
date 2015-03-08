615fe21464d2210c0e2c666b6572474689a8756e
Kevin Iverson <kgiverson@yahoo.com>
2015-03-07 20:48:22 -0600

    Added grid line data at clash level

--- clash_util.py
+++ clash_util.py
@@ -27,12 +27,16 @@ for item in doc.iterfind('batchtest/clashtests/clashtest/clashresults/clashresul
         else:
             file_keys += "-" + sys_elem[0]
 
+    grid_location = ""
+    for grid_line in item.findall('gridlocation'):
+        grid_location = grid_line.text.split(':')[0]
+
     for classp in item.findall('clashpoint/pos3f'):
         x = classp.attrib['x']
         y = classp.attrib['y']
         z = classp.attrib['z']
         coord = (x,y,z)
-        parsed_data[coord] = (imagefile,name,file_keys)
+        parsed_data[coord] = (imagefile,name,file_keys, grid_location)
 
 #now let's iterate and find the ones within a certain distance
 #TODO - make this a command line parameter
@@ -44,16 +48,18 @@ for x in parsed_data:
     clash1_image = parsed_data[x][0]
     clash1_name = parsed_data[x][1]
     clash1_file_key = parsed_data[x][2]
+    clash1_grid_line = parsed_data[x][3]
     z1 = x[2]
     y1 = x[1]
     x1 = x[0]
     finds_key = [clash1_name]
     finds_source = clash1_name
-    finds_data = [group_size, group_file_key, clash1_name, (clash1_name, clash1_image)]
+    finds_data = [group_size, group_file_key, clash1_name, (clash1_name, clash1_image, clash1_grid_line)]
     for y in parsed_data:
         clash2_image = parsed_data[y][0]
         clash2_name = parsed_data[y][1]
         clash2_file_key = parsed_data[y][2]
+        clash2_grid_line = parsed_data[y][3]
         if clash1_name == clash2_name:
             continue
         if clash1_file_key != clash2_file_key:
@@ -69,7 +75,7 @@ for x in parsed_data:
                 xdelt = float(x1)-float(x2)
                 if (xdelt >= -box_size) and (xdelt <= box_size):
                     finds_key.append((clash2_name))
-                    finds_data.append((clash2_name, clash2_image))
+                    finds_data.append((clash2_name, clash2_image, clash2_grid_line))
                     finds_data[0] += 1
     if len(finds_key) > 1:
         s_finds_key = sorted(finds_key)
